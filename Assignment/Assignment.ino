2845d19cd415138e94ca73881fb68e22c3229464
Spoon <spoonnosaurus@gmail.com>
2023-05-02 18:34:15 +0100

    Working number transfer

---
+++ Assignment/Assignment.ino
@@ -0,0 +1,290 @@
+/*
+  Communications v3
+
+  Transmits data using a white LED and recieves it using a photoresistor
+
+*/
+
+int ledState = LOW; // ledState used to set the LED
+
+// the setup routine runs once when you press reset:
+void setup()
+{
+
+  // initialize pins as input
+
+  pinMode(2, INPUT);
+  pinMode(4, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+
+  // set the digital pin as output:
+
+  pinMode(3, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+const long txInterval = 150; // interval at which to tx bit (milliseconds)
+int tx_state = 0;
+char chr;
+unsigned long previousTxMillis = 0; // will store last time LED was updated
+
+#define TX_START_OF_TEXT -1
+int tx_string_state = TX_START_OF_TEXT;
+
+#define STX 0x70
+#define ETX 0x71
+
+char txButton, txTilt, txPot, txA, txB, txC, txD;
+char txBuffer[8] = {0, 0, 0, 0, 0, 0, 0, ETX};
+char rxBuffer[7];
+char rxButton, rxTilt, rxPot, rxA, rxB, rxC, rxD;
+int rx_index;
+
+void readInputs()
+{
+  // Reads the inputs in the mini-projects
+  // Uses txButton, txTilt, txPot, txA, txB, txC, txD;
+
+  txButton = 1;
+  txTilt = 1;
+  txPot = 7;
+  txA = 99;
+  txB = 99;
+  txC = 99;
+  txD = 99;
+  // Serial.print(txButton);
+  // Serial.print(txTilt);
+  // Serial.print(txPot);
+  // Serial.print(txA);
+  // Serial.print(txB);
+  // Serial.print(txC);
+  // Serial.print(txD);
+  // Serial.println();
+}
+
+void writeOutputs()
+{
+  // Writes the outputs in the mini-projects
+  // Uses rxButton, rxTilt, rxPot, rxA, rxB, rxC, rxD;
+
+  digitalWrite(9, rxButton);
+  digitalWrite(10, rxTilt);
+  analogWrite(11, rxPot);
+  digitalWrite(12, rxA);
+  digitalWrite(13, rxB);
+  digitalWrite(A2, rxC);
+  digitalWrite(A3, rxD);
+
+  // Serial.print(rxButton);
+  // Serial.print(rxTilt);
+  // Serial.print(rxPot);
+  // Serial.print(rxA);
+  // Serial.print(rxB);
+  // Serial.print(rxC);
+  // Serial.print(rxD);
+  // Serial.println();
+}
+
+char getTxChar()
+{
+  char chr;
+
+  switch (tx_string_state)
+  {
+  case TX_START_OF_TEXT:
+    tx_string_state = 0;
+    txBuffer[0] = txButton;
+    txBuffer[1] = txTilt;
+    txBuffer[2] = txPot;
+    txBuffer[3] = txA;
+    txBuffer[4] = txB;
+    txBuffer[5] = txC;
+    txBuffer[6] = txD;
+    return STX;
+    break;
+
+  default:
+    chr = txBuffer[tx_string_state];
+    tx_string_state++;
+    if (chr == ETX)
+    { /* End of string? */
+
+      tx_string_state = TX_START_OF_TEXT; /* Update the tx string state to start sending the string again */
+      return ETX;                         /* Send End of Text character */
+    }
+    else
+    {
+      return chr; /* Send a character in the string */
+    }
+    break;
+  }
+}
+
+void txChar()
+{
+  unsigned long currentTxMillis = millis();
+
+  if (currentTxMillis - previousTxMillis >= txInterval)
+  {
+    // save the last time you blinked the LED (improved)
+    previousTxMillis = previousTxMillis + txInterval; // this version catches up with itself if a delay was introduced
+
+    switch (tx_state)
+    {
+    case 0:
+      chr = getTxChar();
+      //Serial.println(chr);
+      digitalWrite(3, HIGH); /* Transmit Start bit */
+      tx_state++;
+      break;
+
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+      if ((chr & 0x40) != 0)
+      { /* Transmit each bit in turn */
+        digitalWrite(3, HIGH);
+      }
+      else
+      {
+        digitalWrite(3, LOW);
+      }
+      chr = chr << 1; /* Shift left to present the next bit */
+      tx_state++;
+      break;
+
+    case 8:
+      digitalWrite(3, HIGH); /* Transmit Stop bit */
+      tx_state++;
+      break;
+
+    default:
+      digitalWrite(3, LOW);
+      tx_state++;
+      if (tx_state > 20)
+        tx_state = 0; /* Start resending the character */
+      break;
+    }
+  }
+}
+
+const long rxInterval = 15; // interval at which to read bit (milliseconds)
+int rx_state = 0;
+char rx_char;
+unsigned long previousRxMillis = 0; // will store last time LED was updated
+int rx_bits[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void rxChar()
+{
+  unsigned long currentRxMillis = millis();
+  int sensorValue;
+  int i;
+
+  if (currentRxMillis - previousRxMillis >= rxInterval)
+  {
+    // save the last time you read the analogue input (improved)
+    previousRxMillis = previousRxMillis + rxInterval; // this version catches up with itself if a delay was introduced
+
+    sensorValue = analogRead(A0);
+    // Serial.println(rx_state);
+
+    switch (rx_state)
+    {
+    case 0:
+      if (sensorValue >= 900)
+      {
+        rx_bits[0]++;
+        rx_state++;
+      }
+      break;
+
+    case 100:
+      if ((rx_bits[0] >= 6) && (rx_bits[8] >= 6))
+      { /* Valid start and stop bits */
+        rx_char = 0;
+
+        for (i = 1; i < 8; i++)
+        {
+          rx_char = rx_char << 1;
+          if (rx_bits[i] >= 6)
+            rx_char = rx_char | 0x01;
+        }
+
+        switch (rx_char)
+        {
+        case STX:
+          rx_index = 0;
+          break;
+
+        case ETX:
+          rxButton = rxBuffer[0];
+          rxTilt = rxBuffer[1];
+          rxPot = rxBuffer[2];
+          rxA = rxBuffer[3];
+          rxB = rxBuffer[4];
+          rxC = rxBuffer[5];
+          rxD = rxBuffer[6];
+
+          
+
+          rx_index = 0;
+          break;
+
+        default:
+          rxBuffer[rx_index] = rx_char;
+          rx_index++;
+          break;
+        }
+        int num_recieved = rx_char;
+        Serial.println(num_recieved); /* Print the recieved character on the monitor - debug purposes */
+      }
+      else
+      {
+        Serial.println("Rx error");
+      }
+      // for (i = 0; i < 10; i++) /* Print the recieved bit on the monitor - debug purposes */
+      // {
+      //   Serial.println(rx_bits[i]);
+      // }
+      for (i = 0; i < 10; i++)
+      {
+        rx_bits[i] = 0;
+      }
+      rx_state = 0;
+      break;
+
+    default:
+      if (sensorValue >= 900)
+      {
+        rx_bits[rx_state / 10]++;
+      }
+      rx_state++;
+      break;
+    }
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+
+  txChar();
+  rxChar();
+  writeOutputs();
+  readInputs();
+}
